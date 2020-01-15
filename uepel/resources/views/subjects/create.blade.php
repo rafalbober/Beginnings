5eb6488689541e1d22bae6a48ae5bde71983fd4a
katherink <katarzyna.kaminska111@gmail.com>
2020-01-15 12:01:41 +0100

    quick fix adding new subject

--- uepel/resources/views/subjects/create.blade.php
+++ uepel/resources/views/subjects/create.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <h2>New book:</h2>
+            <h2>New Subject</h2>
             @if( $errors->any() )
                 <div class="alert alert-danger">
                     <ul>
