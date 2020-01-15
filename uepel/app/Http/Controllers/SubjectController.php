6c6bc19d92ded992fc338cf1c44cf7243fc58cc7
katherink <katarzyna.kaminska111@gmail.com>
2020-01-15 11:52:55 +0100

    attempt for adding new subjects

--- uepel/app/Http/Controllers/SubjectController.php
+++ uepel/app/Http/Controllers/SubjectController.php
@@ -17,4 +17,23 @@ class SubjectController extends Controller
     {
         return view('subjects.create');
     }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+
+            'name' => 'required',
+            'signup_capacity' => 'required'
+
+        ]);
+
+
+        $Subject= new Subjects();
+
+        $Subject->__set('name', $request->input('name'));
+        $Subject->__set('signup_capacity', $request->input('signup_capacity'));
+        $Subject->save();
+
+        return redirect('Subjects/' . $Subject->__get('id'));
+    }
 }
