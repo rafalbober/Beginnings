a9060f8b3f2c18b1860805f2856ea171acf16ca0
katherink <katarzyna.kaminska111@gmail.com>
2020-01-19 04:38:32 +0100

    login views for admin, teacher and student

--- uepel/resources/views/auth/student_login.blade.php
+++ uepel/resources/views/auth/student_login.blade.php
@@ -1,73 +1,83 @@
+<head>
+    <style>
+        body{
+            background-image: url({{ URL::asset('images/b1.jpg') }});
+            background-attachment: fixed;
+            background-size: cover;
+
+        }
+        h1{
+            color: white;
+            margin-top: 40px;
+        }
+    </style>
+</head>
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    <div class="container-fluid" s>
+        <div class="row justify-content-center">
+            <div class="col-md-4" style="background: rgba(0,0,0,0.4); margin-top: 90px; height: 500px; box-shadow: 1px 4px 40px black">
+                <div class="">
+                    <div class=""><h2 class="text-center text-white-50" style="font-weight: 700;padding-top: 10px">Login Student</h2></div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('student.login.submit') }}">
-                        @csrf
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('student.login.submit') }}">
+                            @csrf
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            <div class="form-group row" style="margin-top: 20px;">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="E-mail adress" style="background: transparent; border-radius: 0px; border: 0px; border-bottom: 1px solid gray; color: white; height: 40px; padding:20px; font-size: 20px " >
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
+                                <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
-                        </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
+                            <div class="form-group row">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Password" style="background: transparent; border-radius: 0px; border: 0px; border-bottom: 1px solid gray; color: white; height: 40px; padding:20px; font-size: 20px ">
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
+                                <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
-                        </div>
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
+
+                            <div class="form-group row">
+                                <div class="col-md-12 offset-md-12">
+
+                                    <div class="form-check ">
+                                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                        <label class="form-check-label" for="remember" style="font-size: 15px; color: #ebebeb; letter-spacing:1px">
+                                            {{ __('Remember Me') }}
+                                        </label>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary" id="login_button">
-                                    {{ __('Login') }}
-                                </button>
 
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
+                            <div class="form-group row">
+                                <div class="col-md-12 text-center" style="padding-top: 15%">
+                                    <button type="submit" class="btn" style="font-size: 30px ;background-color:#ebebeb; background-opacity:10%; border: 1px #ebebeb;width: 50%" id="login_button">
+                                        {{ __('Sign in') }}
+                                    </button>
+
+                                    @if (Route::has('password.request'))
+                                        <a class="btn btn-lg" href="{{ route('password.request') }}">
+                                            {{ __('Forgot Your Password?') }}
+                                        </a>
+                                    @endif
+                                </div>
                             </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
+
--- uepel/resources/views/layouts/app.blade.php
+++ uepel/resources/views/layouts/app.blade.php
@@ -27,11 +27,12 @@
     </style>
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
+
+    <div id="app" >
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm" >
+            <div class="container-fluid" >
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    Üpel
+                    Üpelllllllllllllll
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
