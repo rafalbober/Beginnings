0042cc5a5716123d7e13157d7d66e8e79b120761
katherink <katarzyna.kaminska111@gmail.com>
2020-01-20 23:00:11 +0100

    lesson view update

--- uepel/resources/views/lessons/show.blade.php
+++ uepel/resources/views/lessons/show.blade.php
@@ -6,21 +6,52 @@
 <div class="container">
     <div class="row" style=" margin-right: 10% ">
 
-            <div class="media" style="padding-top: 2%;padding-bottom: 2% ">
+            <div class="media" style="padding-top: 2%;">
 
                 <div class="media-body" >
 
-                    <div class="card" style="margin-top: 3%">
-                        <div class="media" >
+                    <div class="card" style="">
+                        <div class="media mb-5" >
                             <img src="{{URL::asset('/images/subject.jpg')}}" width="30%"  >
                             <div class="media-body text-muted" style="margin-left: 2%; margin-top: 1%">
                                 <h2>{{ $lesson->title }} </h2>
                                 <p>{{ $lesson->description }}</p>
-                                <a class="btn btm-md " style="background-color: #D3D3D3;" href="{{route('lesson.edit',$lesson->id)}}">edit</a>
+                               <div class="col-lg-12" style="margin-top: 5%">
+
+                                <div class="dropdown ">
+                                    <button class="btn btn-secondary dropdown-toggle" type="button" id="list_students" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Students who joined the course
+                                    </button>
+                                    <div class="dropdown-menu" aria-labelledby="list_students">
+                                        <?php
+                                        $Student = \App\Student::all();
+
+                                        foreach ($Student as $student)
+                                        {
+                                            echo "<p class='dropdown-item' >$student->name</p>";
+
+                                        }
+
+                                        ?>
+
+                                    </div>
+                                    <a class="btn btm-md " style="margin-left:2%;background-color: #D3D3D3;" href="{{route('lesson.edit',$lesson->id)}}">Edit Lesson</a>
+                                </div>
+                               </div>
+
+
+
+
+
+
+
+
 
 
                             </div>
                         </div>
+                    </div>
+
                     </div>
 
                 </div>
@@ -37,21 +68,6 @@
 
 
 
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <h2>{{ $lesson->title }} </h2>
-            <p>{{ $lesson->description }}</p>
-            <a href="{{route('lesson.edit',$lesson->id)}}">edit</a>
-        </div>
 
     </div>
-    <?php
-    $Student = \App\Student::all();
-        foreach ($Student as $student)
-            {
-                echo "<div class = 'row'>$student->name</div>";
-            }
-
-    ?>
-</div>
 @endsection
