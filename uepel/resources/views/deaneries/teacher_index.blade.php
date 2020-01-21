c1f3610a6b115308a032f5120cd5eb49bec2fcf0
katherink <katarzyna.kaminska111@gmail.com>
2020-01-21 01:22:10 +0100

    teacher-index change

--- uepel/resources/views/deaneries/teacher_index.blade.php
+++ uepel/resources/views/deaneries/teacher_index.blade.php
@@ -5,7 +5,7 @@
         <div>
             <div class="col-md-6" style="margin-bottom: 2%">
                 <h2>Teachers</h2>
-                <a class="btn btm-md " style="background-color: #D3D3D3; font-size: 13px " href={{route('deaneries.student_create')}}>Create new Teacher</a>
+                <a class="btn btm-md " style="background-color: #D3D3D3; font-size: 13px " href={{route('deaneries.teacher_create')}}>Create new Teacher</a>
             </div>
         </div>
         <div class="col-md-8" style=" margin-right: 10% ">
