f10c74d0a97e8db9ba89d8134c20423487b13d77
LBAtla <lukasz.balaa@gmail.com>
2020-01-22 00:44:58 +0100

    Subject desc added

--- uepel/resources/views/subjects/index.blade.php
+++ uepel/resources/views/subjects/index.blade.php
@@ -20,7 +20,7 @@
                                 <h2 style="border-radius: 60px;color: #fff0ff;background-color: #3f3f3f; border: solid #3f3f3f; font-size: 17px;text-align: center; padding: 5px; position: absolute; width: 85% ; margin: 0; bottom: -25px; left:8%;letter-spacing: 2px ">{{ $subject->name }}</h2>
                             </div>
                             <div class="subject-description" style="padding:30px 30px 20px;font-size: 15px;font-weight: 400">
-                                <p style="padding-top: 10%">sth abuout the subject idk loremm lorem weuyrter wuegr ewyr weygruweyt wueygr weugrs fwye fweyf </p>
+                                <p style="padding-top: 10%">{{$subject->description}}</p>
                                 <a class="btn btm-md " style="background-color: #D3D3D3;" href={{route("teacher.request",$subject->id)}}>requests</a>
                                 <a class="btn btm-md " style="background-color: #D3D3D3;" href={{route("subject.show",$subject->id)}}>more details</a>
                             </div>
