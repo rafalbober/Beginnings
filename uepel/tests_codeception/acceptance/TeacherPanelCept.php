596c1c3b2f9c36bcab8140b946dfeb0c5a13c938
rafalbober <rafaubober@gmail.com>
2020-01-22 00:18:34 +0100

    TeacherPanelCept.php

--- uepel/tests_codeception/acceptance/TeacherPanelCept.php
+++ uepel/tests_codeception/acceptance/TeacherPanelCept.php
@@ -1,3 +1,36 @@
-<?php 
+<?php
 $I = new AcceptanceTester($scenario);
-$I->wantTo('perform actions and see result');
+$I->wantTo('see all links');
+
+$I->amOnPage('/teachers/home');
+$I->fillField('email', 'siwy@ggios.pl');
+$I->fillField('password', 'hahaha');
+$I->click('#login_button');
+$I->see("Teacher Panel");
+
+$I->seeLink("Teacher Home", "/teachers/home");
+$I->seeLink("About me", "/teachers/index/1");
+$I->seeLink("My Subjects", "/subjects/1");
+$I->seeLink("Add new Subject", "/subjects/create");
+
+
+$I->wantTo("use all links on Teacher Panel page");
+$I->click("About me");
+$I->seeCurrentUrlEquals("/teachers/index/1");
+
+$I->click("Teacher Home");
+
+$I->click("My Subjects");
+$I->seeCurrentUrlEquals("/subjects/1");
+
+$I->click("Teacher Home");
+
+$I->click("Add new Subject");
+$I->seeCurrentUrlEquals("/subjects/create");
+
+$I->click("Teacher Home");
+
+$I->wantTo("Logout");
+$I->click("Zdzisław");
+//$I->click("Logout");
+//$I->seeCurrentUrlEquals("/teachers/login");
