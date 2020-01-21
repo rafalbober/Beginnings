99a49a08fd1347eb5638de66d61c6e3f0b1067a5
ptrkfrsk <pfrasik1@wp.pl>
2020-01-21 23:07:17 +0100

    Lesson forms title. Create lesson tmp fix

--- uepel/resources/views/lessons/edit.blade.php
+++ uepel/resources/views/lessons/edit.blade.php
@@ -23,7 +23,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-6" style="background: rgba(0,0,0,0.4); margin-top: 2%;  box-shadow: 1px 4px 40px black">
-            <h2 class="text-center" style="margin: 3%">Edit subject:</h2>
+            <h2 class="text-center" style="margin: 3%">Edit lesson:</h2>
             @if( $errors->any() )
                 <div class="alert alert-danger">
                     <ul>
