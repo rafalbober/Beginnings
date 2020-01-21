13824d27911ab5aba1acd5d624c4e8722ce1d7c4
katherink <katarzyna.kaminska111@gmail.com>
2020-01-21 17:36:29 +0100

    studenyt panel change

--- uepel/resources/views/students/home.blade.php
+++ uepel/resources/views/students/home.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Üpel Teacher</title>
+    <title>Üpel Student</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -13,6 +13,87 @@
 
 
 <body>
+
+
+
+
+
+
+
+
+
+
+<div class="container-fluid mb-12">
+    <div class="row justify-content-center "  >
+        <div class=" text-center " >
+            <h1></h1>
+            <br style="font-weight: 500; font-size: 60px"> </br></br> </h2>
+
+        </div>
+    </div>
+
+    <div class="container-fluid mb-5 justify-content-center"  >
+
+        <div class="row justify-content-center">
+            <div class="col-md-6 justify-content-center" style="background: #ffffff">
+                <div class="card md-12">
+                    <div class="card-body text-center">
+                        <h2 class="col-md-12 text-center">Student Panel</h2>
+
+                        <img src="{{URL::asset('/images/avatar4.jpeg')}}" class="rounded-circle">
+                        <h3 class="card-title mt-3">  {{$student->name}}</h3>
+                        <h4 class="card-title mt-3">  {{$student->email}}</h4>
+
+
+
+                        <h5 style="margin: 2%">
+                            Choose users to edit
+                        </h5>
+                        <div class="col mb-12">
+                            <a class="btn btm-md " style="background-color: #D3D3D3; margin: 5%; width: 80%" href="{{route('student.index',$student->id)}}" >About me </a> </div>
+                        <div class="col mb-12">
+                            <a class="btn btm-md " style="background-color: #D3D3D3; margin: 5%; width: 80%" href="{{route('student.my_subjects')}}">My Subjects</a> </div>
+                        <div class="col mb-12">
+                            <a class="btn btm-md " style="background-color: #D3D3D3; margin: 5%; width: 80%" href="{{route('student.subjects_show')}}"> Available Subjects</a> </div>
+
+
+                    </div>
+                </div>
+
+            </div>
+
+
+
+        </div>
+    </div>
+
+</div>
+
+</div>
+</div>
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--
+
 <div class="container-fluid mb-12">
     <div class="row justify-content-center "  >
         <div class=" text-center " >
@@ -33,9 +114,7 @@
                         <img src="{{URL::asset('/images/avatar4.jpeg')}}" class="rounded-circle">
                         <h3 class="card-title mt-3">  {{$student->name}}</h3>
                         <h4 class="card-title mt-3">  {{$student->email}}</h4>
-                        <p>
-                            I admire good jokes, passats and some lemonade.
-                        </p>
+
                         <div class="col mb-12">
                             <a class="btn btm-md " style="background-color: #D3D3D3; margin: 5%; width: 80%" href="{{route('teacher.index',$student->id)}}" >About me </a> </div>
                         <div class="col mb-12">
@@ -71,7 +150,7 @@
 </body>
 
 </html>
-
+-->
 <!--
 <body>
 <div class="flex-center position-ref full-height">
